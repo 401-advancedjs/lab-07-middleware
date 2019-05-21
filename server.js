9968c8b2c76e661802912d4a5110add1afb8554e
mestock <MelissaStock44@gmail.com>
2019-05-21 14:44:56 -0700

    added error handler and not found middleware

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+const notFound = (req, res) => {
+  console.log('Hello from not found');
+  res.status(404).send('testing');
+};
+
+
 app.get('/a', (req,res) => {
   res.status(200).send('Route A');
 });
@@ -22,5 +28,14 @@ app.get('/d', (req,res) => {
   res.status(200).send('Route D');
 });
 
+app.use('*', notFound);
+
+
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
+const errorHandler = (err, req, res, next) => {
+  console.log('Made it into the errorHandler');
+  res.status(404).send(err);
+};
+
+app.use(errorHandler);
