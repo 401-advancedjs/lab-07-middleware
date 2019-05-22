7339a1d5df71309a67584a602031169d36cd9db8
mestock <MelissaStock44@gmail.com>
2019-05-22 12:45:11 -0700

    fixed path finding bugs

--- server.js
+++ server.js
@@ -6,27 +6,26 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-const router = require('./lib/router.js/index.js');
+const router = require('./lib/router.js');
 const reqTime = require('./lib/reqTime.js');
 const logger = require('./lib/logger.js');
 const errorHandler = require('./lib/errorHandler.js');
-const squareIt = require('./lib/sqaureIt.js');
+const squareIt = require('./lib/squareIt.js');
 const notFound = require('./lib/notFound.js');
 
-app.use(logger);
 app.use(reqTime);
-app.use(squareIt);
-app.use(notFound);
+app.use(logger);
 
-app.get('/a', reqTime, logger, (req, res, next) => {
+app.get('/a', (req, res, next) => {
   res.status(200).send('Route A');
 });
 
-app.get('/b/:num', reqTime, logger, squareIt, (req, res, next) => {
+app.get('/b/:num', squareIt, (req, res, next) => {
   res.send({number: req.number});
 });
 
 app.use(router);
+app.use(notFound);
 app.use('*', notFound);
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
