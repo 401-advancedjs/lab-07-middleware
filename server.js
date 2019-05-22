117acd849d40c0cce185b88eea124c8d11ebc547
mestock <MelissaStock44@gmail.com>
2019-05-21 17:30:57 -0700

    modularize paths and middleware

--- server.js
+++ server.js
@@ -6,21 +6,19 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+const router = require('./router.js');
+const reqTime = require('./reqTime.js');
+const logger = require('./logger.js');
+const errorHandler = require('./errorHandler.js');
+
+app.use(logger);
+app.use(reqTime);
+
 const notFound = (req, res) => {
-  console.log('Hello from not found');
-  res.status(404).send('testing');
+  console.log('Path not found, please enter valid path.');
+  res.status(404).send('Path not found, please enter valid path.');
 };
 
-const reqTime = (req, res, next) => {
-  console.log('hello from reqTime');
-  req.requestTime = new Date();
-  next();
-};
-
-const logger = (req, res, next) => {
-  console.log('Method: ', req.method, 'Path: ', req.path, 'Time: ', req.requestTime);
-  next();
-};
 
 const squareIt = (req, res, next) => {
   let num = req.params.num;
@@ -29,32 +27,19 @@ const squareIt = (req, res, next) => {
   next();
 };
 
-
 app.get('/a', reqTime, logger, (req, res, next) => {
   res.status(200).send('Route A');
 });
 
 app.get('/b/:num', reqTime, logger, squareIt, (req, res, next) => {
-  console.log(req.number);
   res.send({number: req.number});
 });
 
-app.get('/c', reqTime, logger, (req, res, next) => {
-  res.status(200).send('Route C');
-});
-
-app.get('/d', reqTime, logger, (req, res, next) => {
-  res.status(200).send('Route D');
-});
+app.use(router);
 
 app.use('*', notFound);
 
-
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const errorHandler = (err, req, res, next) => {
-  console.log('Made it into the errorHandler');
-  res.status(404).send(err);
-};
-
 app.use(errorHandler);
+
